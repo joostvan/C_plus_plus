6f0d9cdd646166d67d41f53892f89d847c3c2d0c
Joost <jvanderborgh@gmail.com>
2024-04-13 19:47:59 -0400

    lecture 2

---
+++ lecture 2/references2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+)
+
+void F(int x){
+    x = x + 6;
+}
+
+void G(int& x){
+    x = x + 10;
+}
+
+int main(){
+    int Z {0};
+    std::cout << "Z = " << Z << std::endl;
+    F(Z);
+    std::cout << "Z = " << Z << std::endl;
+    G(Z);
+    std::cout << "Z = " << Z << std::endl;
+    int& W { Z };
+    G(W);
+    std::cout << " Z = " << Z << std::endl;
+}
---
+++ lecture 2/second_law_B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+)
+int main(){
+    std::vector<int> V {6, 10, 17};
+    std::vector<int> W P{};
+    W = V
+    V.at(0) = 999
+    W.push_back(-116);
+    std::cout << "V: ";
+    print_vector(V);
+    std::cout << "W: ";
+    print_vector(W);
+    return 0;
+}
