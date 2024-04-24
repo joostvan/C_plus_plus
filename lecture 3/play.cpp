557af2ce40b41b6c3490bdc2cb0eb0b0cb15eadc
Joost <jvanderborgh@gmail.com>
2024-04-24 16:55:16 -0400

    what time is it?

---
+++ lecture 3/play.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int value1 = 5;
+    int value2 = 10;
+
+    if (value1 > value2) {
+        std::cout << "value1 is greater than value2." << std::endl;
+    } else {
+        std::cout << "value1 is not greater than value2." << std::endl;
+    }
+
+    return 0;
+}
--- lecture 3/what_time.cpp
+++ lecture 3/what_time.cpp
@@ -12,27 +12,55 @@ void print_time( TimeOfDay the_time){
     std::cout << the_time.hour << ":" << the_time.minute;
 }
 
-/* parse_time(input)
-Given A string attempt to pour the contents into an hour and minute and return a time of day object containing the result.
-A string attempt to pour the contents into an hour and minute and return a time of day object containing the result.
 
-Exception should be thrown in the following cases cases must be checked in the order below
-If the string does not contain two numerical field, separated by colon e.g. 1006, not six or 10, throw std::domain_error (NOT std::invalid_argument)
+TimeOfDay parse_time(std::string input){
+    std::string before_colon {};
+    std::string after_colon {};
+    bool found_colon{false};
+    for (auto c: input){
+        if (c==':'){
+            found_colon = true;
+        } else if (found_colon){
+            after_colon += c;
+        } else {
+            before_colon += c;
+        }
+    }
+    std::cout << "BC: " << before_colon << std::endl;
+    std::cout << "AC: " << after_colon << std::endl;
 
-If the hour is not in the range, 0 to 23 or the minute is not in the range 0-59 STD:: out of range
+    if (!found_colon){
+        throw std::domain_error{"Invalid time or no colon!"};
+    } 
+    int hour {}, minute {};
+    try {
+        hour = std::stoi(before_colon);
+        minute = std::stoi(after_colon);
+        } catch (std::invalid_argument& e){
+            throw std::domain_error {"invalid time"};
+        }
+    if (hour < 0 || hour > 23 || minute < 0 || minute > 59){
+        throw std::out_of_range{"Invalid time"};
+    }
 
-*/
+    TimeOfDay result {6, 10};
+    result.hour = hour;
+    result.minute = minute;
 
-// homework
+    TimeOfDay result {(unsigned int)hour, (unsigned int)minute}
+    return result;
+
+}
+
 int main(){
     std::string input_string;
     std::cout << "What time is it?";
     std::getline(std::cin, input_string);
-    std::cout << "You entered "<< input_string << std:endl;
+    std::cout << "You entered "<< input_string << std::endl;
     // Task handle error cases
-    TimeOfday T {parse_time(input_string)};
-    std::cout << "The time is ";
+    TimeOfDay T {parse_time(input_string)};
+    //std::cout << "The time is ";
     print_time(T);
     std::cout << std::endl;
     return 0;
-}
+} 
