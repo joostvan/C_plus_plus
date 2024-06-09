0f27ee1030ad79061d09fcc2a8eb7b90b5346b77
Joost <jvanderborgh@gmail.com>
2024-06-09 10:47:11 +0200

    message

---
+++ lecture 6/rectangle_scruct.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+// buckets of data
+
+struct Rectangle {
+    double width {0};
+    double height {0};
+};
+
+double get_rectangle_area(Rectangle& rec) {
+    return rec.width * rec.height;
+}
+
+int main() {
+    Rectangle R{1, 1};
+    Rectangle R2 {6, 10};
+    std::cout << "R has width " << R.width << std::endl;
+    std::cout << "R has height " << R.height << std::endl;
+    std::cout << "R has area " << get_rectangle_area(R) << std::endl;
+    return 0;
+}
